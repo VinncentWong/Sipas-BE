b2acaff3a14a4c2532cef6123d86352dfbe4fc6f
VinncentWong <vinncentwong2@gmail.com>
2023-08-13 21:38:13 +0700

    initial commit

---
+++ src/main/java/bcc/sipas/SipasApplication.java
@@ -0,0 +1,13 @@
+package bcc.sipas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SipasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SipasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bcc/sipas/SipasApplicationTests.java
@@ -0,0 +1,13 @@
+package bcc.sipas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SipasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
