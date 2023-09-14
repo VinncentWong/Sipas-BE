62d57b07fed79d5d897fbb65af1db771544501b1
VinncentWong <vinncentwong2@gmail.com>
2023-09-14 14:36:54 +0700

    [ADD] new constant

---
+++ src/main/java/bcc/sipas/constant/BayiAnakConstant.java
@@ -0,0 +1,5 @@
+package bcc.sipas.constant;
+
+public class BayiAnakConstant {
+    public static final String BAYI_ANAK = "bayi_anak";
+}
---
+++ src/main/java/bcc/sipas/constant/KehamilanConstant.java
@@ -0,0 +1,5 @@
+package bcc.sipas.constant;
+
+public class KehamilanConstant {
+    public static final String IBU_HAMIL = "ibu_hamil";
+}
