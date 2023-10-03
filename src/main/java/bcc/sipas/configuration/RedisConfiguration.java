aa5b7a22976fe15a738ffcd031355e885d7f3dd4
VinncentWong <vinncentwong2@gmail.com>
2023-10-03 18:00:55 +0700

    [ADJUST] configuration

--- src/main/java/bcc/sipas/configuration/RedisConfiguration.java
+++ src/main/java/bcc/sipas/configuration/RedisConfiguration.java
@@ -8,6 +8,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
@@ -28,10 +30,15 @@ public class RedisConfiguration {
     @Value("${redis.port}")
     private Integer port;
 
+    @Value("${redis.password}")
+    private String password;
+
     @Bean
     public LettuceConnectionFactory redisClientConfiguration(){
+        var connection = new RedisStandaloneConfiguration(this.host, this.port);
+        connection.setPassword(RedisPassword.of(password.isEmpty() ? "" : password));
         return new LettuceConnectionFactory(
-                new RedisStandaloneConfiguration(this.host, this.port)
+                connection
         );
     }
 
